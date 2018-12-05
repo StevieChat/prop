abc572b9d7db17b7868be9e3595c5788552132d9
Dylan Redman <dylan.j.redman@gmail.com>
2018-12-04 22:11:52 -0600

    update to Question Component

---
+++ src/Components/Question/Question.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './InteractionWindow.module.css';
+
+const interactionWindow = (props) => {
+    return (
+        <div className={styles.InteractionWindow}></div>
+    );
+};
+
+export default interactionWindow;
