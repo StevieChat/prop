5dc33e81d7d8cc3260dcc0adeaa7416482e2ce34
Dylan Redman <dylan.j.redman@gmail.com>
2018-12-04 21:46:20 -0600

    Added Base Layout

---
+++ src/HOC/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+import InteractionWindow from '../../Components/InteractionWindow/InteractionWindow';
+
+class Layout extends Component {
+    render(){
+        return (
+            <InteractionWindow></InteractionWindow>
+        );
+    }
+}
+
+export default Layout;
